1f9638042dcd7b72e84d7a2f534c62bccf05bbab
Mohamed Ifah <ifaa.x.h@gmail.com>
2015-02-05 12:44:29 +0500

    source code of CSS Fullscreen Background Image
    Add an image to your website background and have it stretch so it always
    fills the viewport. We'll also add some text to finish it off nicely.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>CSS Fullscreen Background</title>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Amatic+SC">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+  	<h1 class="top-header">Through darkness light is revealed.</h1>
+  </body>
+</html>
